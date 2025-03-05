30f1dd4400a908e09f3beaccc8db19513c1a4dcd
gwolner <guilherme.wdm@gmail.com>
2025-03-04 23:19:07 -0300

    build: Criado ContaSchema

--- src/data/models.js
+++ src/data/models.js
@@ -19,10 +19,28 @@ function InstituicaoModel(options = {}){
  * Modelo de Grupo
  * - Tabela: grupo
  * @param { Object } options 
- * 
+ * `id`, `nome`, `descricao`
  */
 function GrupoSchema(options = {}){
     this.id = options.id;
     this.nome = options.nome;
     this.descricao = options.descricao;
+}
+
+
+/**
+ * Modelo de Conta
+ * - Tabela: conta
+ * @param { Object } options 
+ * `id`, `agencia`, `numero`, `tipo`, `saldo`, `fgc_conta`, `conta_ativa`, `instituicao_ìd`, `grupo_id`
+ */
+function ContaSchema(options = {}){
+    this.id = options.id;
+    this.agencia = options.agencia;
+    this.numero = options.numero;
+    this.tipo = options.tipo;
+    this.saldo = options.fgcConta;
+    this.contaAtiva = options.contaAtiva;
+    this.instituicaoId = options.instituicaoId;
+    this.grupoId = options.grupoId;
 }
