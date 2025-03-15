d9e3030c2640f012c14d51e5f136aca042784ff5
gwolner <guilherme.wdm@gmail.com>
2025-03-15 15:33:48 -0300

    build: Arquivos de apoio

---
+++ src/data/databases.js
@@ -0,0 +1,31 @@
+/** 
+ * #################################
+ * ###### CRIAR A MINHA VERSÃO #####
+ * #################################
+ *  */ 
+const { Pool } = require("pg");
+const configurations = require('../services/configurations/configuration.service')
+
+/**
+ * Obtém a instancia da connection pool
+ * @returns {Pool}
+ */
+function getConnectionPool() {
+  if (global.connectionPool) {
+    return global.connectionPool;
+  }
+
+  const pgConfig = {
+    connectionString: configurations.db.connectionString,
+    ssl: configurations.db.ssl,
+  };
+
+  const connectionPool = new Pool(pgConfig);
+
+  global.connectionPool = connectionPool;
+  return global.connectionPool;
+}
+
+module.exports = {
+  getConnectionPool,
+};
